ea9f8be5f2d61f67ad1a83f6a9574ef652067789
marim0ss <piyomaru.bunbun@gmail.com>
2018-10-10 23:48:09 +0900

    Postモデル作成、devise:views実行でデバイス用のビューも生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'users#index'
+  root to: 'posts#index'
 
   resources :users
     # => index, show, new, create, edit, update, destroyのアクションに対応させることができる
 
+  resources :posts
 
   # resources :users do
   #     resources :posts    #ネストしてuserモデルにpostモデルを紐付ける
